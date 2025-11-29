e3884b3985eafafdbe9e3db3de251479b5800149
sheetal nair <sheetalnair@sheetals-MacBook-Air.local>
2025-11-29 21:00:34 +0530

    otpInput completed

--- src/components/SystemDesign/OtpInput/OtpInput.jsx
+++ src/components/SystemDesign/OtpInput/OtpInput.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import './OtpInput.css';
 
 const OtpInput = () => {
@@ -8,21 +8,50 @@ const OtpInput = () => {
 
     const [inputArr, setInputArr] = useState(new Array(OTP_DIGIT_COUNT).fill('')); //if [], nothing renders intially
 
+    const refArr = useRef([]);
+
+    useEffect(()=> {
+        refArr.current && refArr.current[0].focus();
+    },[])
+
     const handleOnChange = (value,index) => {
-       
+
+     if(isNaN(value)) return; //Nan means Not a number
+     const newValue = value.trim();
+       const newArray = [...inputArr];
+       newArray[index] = newValue.slice(-1); //takes last input ie if you type 394 in same input box, it will take 4 only
+
+       setInputArr(newArray);
+      
+    if (newValue && index < OTP_DIGIT_COUNT - 1) {
+      refArr.current[index + 1].focus();
     }
+
+    }
+
+    const handleKeyDown = (e,index) => {
+        if(!e.target.value && e.key == 'Backspace'){
+           refArr.current[index - 1].focus(); //first comes focus and then deletion ie backspace happens so if we dont put !e.target.value prev value gets deleted
+           // so now if input box is empty only then focus prev index and do backspace/deletion
+        }
+    }
+
     return (
         <div className='container'>
           <h1>Validation OTP</h1>
-          {inputArr.map((item,index)=> (
-            <input 
-                className='otp-input' 
-                key={index}
-                type='number'
-                value={inputArr[index]}
-                onChange= {(e)=> handleOnChange(e.target.value, index)}
-            />
-        ))}
+          <div className='otpBoxes'>
+            {inputArr.map((item,index)=> (
+                <input 
+                    className='otp-input' 
+                    key={index}
+                    type='text'
+                    value={inputArr[index]}
+                    ref= {input => refArr.current[index] = input}
+                    onChange= {(e)=> handleOnChange(e.target.value, index)}
+                    onKeyDown={(e)=> handleKeyDown(e,index)}
+                />
+            ))}
+        </div>
         </div>
     )
 }
