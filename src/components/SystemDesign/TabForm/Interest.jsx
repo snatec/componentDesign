8c4a5f945a9a590342e866802fc7e036f60f0443
sheetal nair <sheetalnair@sheetals-MacBook-Air.local>
2025-11-30 22:48:19 +0530

    modified add remove

--- src/components/SystemDesign/TabForm/Interest.jsx
+++ src/components/SystemDesign/TabForm/Interest.jsx
@@ -6,18 +6,24 @@ const Interest = ({data,setData,error}) => {
     const handleDataChange = (e) => {
     const value = e.target.name; // name of the checkbox // [coding/music/badminton]
     const checked = e.target.checked;
+ setData(prev => ({
+        ...prev,
+        interest: checked
+            ? [...prev.interest, value]              // add
+            : prev.interest.filter(item => item !== value) // remove
+        }));
 
-    setData((prevState) => {
-      const newInterest = checked
-        ? [...prevState.interest, value] // add
-        : prevState.interest.filter((item) => item !== value); // remove // array.filter(callback) // Include the item in the new array only if it is NOT equal (!==) to value.
-// suppose item is coding and user has unchecked coding "coding" !== "coding" false so dont add in array.
+//     setData((prevState) => {
+//       const newInterest = checked
+//         ? [...prevState.interest, value] // add
+//         : prevState.interest.filter((item) => item !== value); // remove // array.filter(callback) // Include the item in the new array only if it is NOT equal (!==) to value.
+// // suppose item is coding and user has unchecked coding "coding" !== "coding" false so dont add in array.
 
-        return {
-        ...prevState,
-        interest: newInterest, // newInterest is the final array
-      };
-    });
+//         return {
+//         ...prevState,
+//         interest: newInterest, // newInterest is the final array
+//       };
+//     });
   };
 
     return(
